8ddc8e5635307d9dc1fe6ed8ab23959149e2fe4f
raintype <raintype@gmail.com>
2016-05-25 11:01:37 +0900

    http://start.spring.io/ 에서 spring Gradle Project 생성

---
+++ src/main/java/com/raintype/CouponBackofficeApplication.java
@@ -0,0 +1,12 @@
+package com.raintype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponBackofficeApplication.class, args);
+	}
+}
