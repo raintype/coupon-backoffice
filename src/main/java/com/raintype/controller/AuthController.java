3eba816893b95f4d2cdc22ef49185d86dbf7b898
raintype <raintype@gmail.com>
2016-06-16 14:08:13 +0900

    인증 관련 interceptor 추가

---
+++ src/main/java/com/raintype/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.raintype.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("admin")
+public class AdminController {
+
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public void loginGet(){
+
+    }
+
+    @RequestMapping(value="/login", method = RequestMethod.POST)
+    public void loginPOST(HttpSession session, Model model) throws Exception{
+
+
+    }
+}
