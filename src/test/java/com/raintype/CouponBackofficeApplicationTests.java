35f48580338579e9ed186a3c2401217a6ea07eee
raintype <raintype@gmail.com>
2016-07-27 09:28:10 +0900

    MySql Connection setting

--- src/test/java/com/raintype/CouponBackofficeApplicationTests.java
+++ src/test/java/com/raintype/CouponBackofficeApplicationTests.java
@@ -2,17 +2,35 @@ package com.raintype;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CouponBackofficeApplication.class)
 @WebAppConfiguration
 public class CouponBackofficeApplicationTests {
+	@Autowired
+	private DataSource ds;
+
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testConnection() throws Exception{
+		System.out.println(ds);
+
+		Connection con = ds.getConnection();
+
+		System.out.println(con);
+
+		con.close();
 	}
 
 }
